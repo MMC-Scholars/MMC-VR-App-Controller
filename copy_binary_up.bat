f1028861bce187cd6c00fc6f627d4b1c4b8dd8f1
MMC-Scholars <latrunkster@gmail.com>
2018-11-13 12:20:32 -0500

    Cleaning repository after accidental project duplication - Michael

--- copy_binary_up.bat
+++
@@ -1,3 +0,0 @@
-:: Use this to copy the built binary to the office's root folder
-::x64\Debug\VRAppController.exe
-copy "x64\Debug\VRAppController.exe" ".."
--- copy_default_list_up.bat
+++
@@ -1,3 +0,0 @@
-:: Use this to copy the built binary to the office's root folder
-::x64\Debug\VRAppController.exe
-copy "vr_apps_list.dat" ".."
